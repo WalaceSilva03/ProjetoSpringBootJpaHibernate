2f27c9669ed4012c220d22ef64484ada93bbcac5
WalaceSilva03 <walace.jose00@gmail.com>
2024-12-09 18:09:08 -0300

    Corrigindo imports

--- src/main/java/com/testespringvscode/projetoitens/Resources/UserResource.java
+++ src/main/java/com/testespringvscode/projetoitens/Resources/UserResource.java
@@ -11,7 +11,6 @@ import com.testespringvscode.projetoitens.services.UserService;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 
