5604cf4c209516edf6d9c7a7518e1e821d80667c
WalaceSilva03 <walace.jose00@gmail.com>
2024-12-09 14:17:18 -0300

    Project created

---
+++ src/main/java/com/testespringvscode/projetoitens/ProjetoitensApplication.java
@@ -0,0 +1,13 @@
+package com.testespringvscode.projetoitens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoitensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoitensApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/testespringvscode/projetoitens/ProjetoitensApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testespringvscode.projetoitens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoitensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
