926833b1d993115614e8a305bdd536fb43b3bf3b
WalaceSilva03 <walace.jose00@gmail.com>
2024-12-11 15:03:07 -0300

    Criando classe Payment e formando a ligação entre as entidades de pagamento e pedidos.

--- src/main/java/com/testespringvscode/projetoitens/config/TestConfig.java
+++ src/main/java/com/testespringvscode/projetoitens/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.testespringvscode.projetoitens.entities.Category;
 import com.testespringvscode.projetoitens.entities.Order;
 import com.testespringvscode.projetoitens.entities.OrderItem;
+import com.testespringvscode.projetoitens.entities.Payment;
 import com.testespringvscode.projetoitens.entities.Product;
 import com.testespringvscode.projetoitens.entities.User;
 import com.testespringvscode.projetoitens.entities.entitiesEnuns.OrderStatus;
@@ -68,9 +69,9 @@ public class TestConfig implements CommandLineRunner {
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 
-        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.WAITING_PAYMENT, u1);
-        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.CANCELED, u2);
-        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.DELIVERED, u1);
+        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
+        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
 
         userRepository.saveAll(Arrays.asList(u1, u2)); // Salva o User 1 e User 2 no banco em forma de lista.
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
@@ -82,6 +83,10 @@ public class TestConfig implements CommandLineRunner {
 
         orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 
+        Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+        o1.setPayment(pay1);
+
+        orderRepository.save(o1);
     }
 }
 // Primeiro caso de injeção de dependência, a classe teste precisa acessar o
